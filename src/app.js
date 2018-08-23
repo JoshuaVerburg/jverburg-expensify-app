4291a032e4ae6d29c5c8d1c6648dd571ffaa0932
Joshua Verburg <joshuapverburg@gmail.com>
2018-08-23 13:29:50 -0600

    Add Expensify styles

--- src/app.js
+++ src/app.js
@@ -11,6 +11,7 @@ import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css'
 import { firebase } from './firebase/firebase';
 import { ifError } from 'assert';
+import LoadingPage from './components/LoadingPage'
 
 const store = configureStore();
 let hasRendered = false;
@@ -26,7 +27,7 @@ const jsx = (
         <AppRouter/>
     </Provider>
 );
-ReactDOM.render(<p>Loading... </p>, document.getElementById('app'));
+ReactDOM.render(<LoadingPage/>, document.getElementById('app'));
 
 
 firebase.auth().onAuthStateChanged((user) => {
--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import { Link } from 'react-router-dom';
 import numeral from 'numeral';
 import totalExpenses from '../selectors/expenses-total';
 import selectExpenses from '../selectors/expenses';
@@ -8,8 +9,14 @@ import selectExpenses from '../selectors/expenses';
 export const ExpensesSummary = (props) => {
     const expenseWord = props.expenses.length === 1 ? 'expense' : 'expenses';
     return(
-        <div>
-            <h1>Viewing {props.expenses.length} {expenseWord} totalling {numeral(totalExpenses(props.expenses)/100).format("$0,0.00")}</h1>
+        <div className="page-header">
+            <div className="content-container">
+                <h1 className="page-header__title">Viewing <span>{props.expenses.length}</span> {expenseWord} totalling <span>{numeral(totalExpenses(props.expenses)/100).format("$0,0.00")}</span></h1>
+                <div className="page-header__actions">
+                    <Link className="button" to="/create">Add Expense</Link>
+                </div>
+            </div>
+            
         </div>
         )
 };
